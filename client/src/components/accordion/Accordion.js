efd421bcd7bf2e5cacf16e5e474b02f4b72b1bc5
Niranjan Kumar <niranjanpid23@gmail.com>
2021-08-21 12:34:20 +0530

    added styling to filters div in shop

--- client/src/components/accordion/Accordion.js
+++ client/src/components/accordion/Accordion.js
@@ -1,13 +1,26 @@
+import React, { useRef } from "react";
 import { ExpandMore } from "@material-ui/icons";
-import React from "react";
+import "./accordion.scss";
 
 const Accordion = ({ title, children }) => {
+  const bottom = useRef();
+
+  const handleShowBottom = () => {
+    if (bottom.current.hasAttribute("style")) {
+      bottom.current.removeAttribute("style");
+    } else {
+      bottom.current.style.maxHeight = bottom.current.scrollHeight + "px";
+    }
+  };
+
   return (
-    <div>
-      <div>
+    <div className="accordion">
+      <div className="accordion__top" onClick={handleShowBottom}>
         {title} <ExpandMore />
       </div>
-      <div>{children}</div>
+      <div ref={bottom} className="accordion__bottom">
+        {children}
+      </div>
     </div>
   );
 };
--- client/src/pages/shop/Shop.js
+++ client/src/pages/shop/Shop.js
@@ -2,52 +2,46 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import { useSelector, useDispatch } from "react-redux";
 import { getByFilter, listProducts } from "../../utils/product";
-import { Slider, Checkbox, Box, Radio } from "@material-ui/core";
+import { Slider, Checkbox, Radio, FormControlLabel } from "@material-ui/core";
 import ProductCard from "../../components/productCard/ProductCard";
 import WarningDiv from "../../components/warning/WarningDiv";
 import Accordion from "../../components/accordion/Accordion";
 import { getCategories } from "../../utils/category";
 import { Rating } from "@material-ui/lab";
-import { labels } from "../../utils/starLabels";
 import "./shop.scss";
 
 const Shop = () => {
   const [products, setProducts] = useState(null);
-  const [price, setPrice] = useState([10, 10]);
-  const [displaySearch, setDisplaySearch] = useState(false);
+  const [price, setPrice] = useState([0, 0]);
   const [categories, setCategories] = useState(null);
   const [categoriesId, setCategoriesId] = useState([]);
   const [shipping, setShipping] = useState();
   const [color, setColor] = useState();
   const [value, setValue] = useState(0);
-  const [hover, setHover] = useState(-1);
   const { search } = useSelector((state) => ({ ...state }));
   const dispatch = useDispatch();
   const { text } = search;
   const colorsData = ["Blue", "Red", "Green", "Black", "Silver"];
 
   useEffect(() => {
-    if (displaySearch === false) {
+    if (text === "") {
       (async () => {
         const res = await listProducts(12);
         setProducts(res.data);
       })();
     }
-  }, [displaySearch]);
+  }, [text]);
 
   useEffect(() => {
     const filteredProducts = setTimeout(async () => {
-      if (text === "") {
-        setDisplaySearch(false);
-      } else {
+      if (text !== "") {
         setCategoriesId([]);
-        setPrice([10, 10]);
+        setPrice([0, 0]);
         setValue(0);
         setShipping();
         setColor();
 
         const res = await getByFilter({ query: text });
-        setDisplaySearch(true);
         setProducts(res.data);
       }
     }, 300);
@@ -71,7 +65,6 @@ const Shop = () => {
     setShipping();
     setColor();
     setValue(0);
-    setDisplaySearch(true);
 
     const res = await getByFilter({ price });
     setProducts(res.data);
@@ -83,7 +76,7 @@ const Shop = () => {
       type: "SEARCH_QUERY",
       payload: { text: "" },
     });
-    setPrice([10, 10]);
+    setPrice([0, 0]);
     setValue(0);
     setShipping();
     setColor();
@@ -106,7 +99,7 @@ const Shop = () => {
       payload: { text: "" },
     });
     setCategoriesId([]);
-    setPrice([10, 10]);
+    setPrice([0, 0]);
     setShipping();
     setColor();
 
@@ -121,7 +114,7 @@ const Shop = () => {
       payload: { text: "" },
     });
     setCategoriesId([]);
-    setPrice([10, 10]);
+    setPrice([0, 0]);
     setValue(0);
     setColor();
 
@@ -136,7 +129,7 @@ const Shop = () => {
       payload: { text: "" },
     });
     setCategoriesId([]);
-    setPrice([10, 10]);
+    setPrice([0, 0]);
     setValue(0);
     setShipping();
 
@@ -145,38 +138,66 @@ const Shop = () => {
     setColor(e.target.value);
   };
 
+  const clearAll = () => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: "" },
+    });
+    setCategoriesId([]);
+    setPrice([0, 0]);
+    setValue(0);
+    setShipping();
+    setColor();
+
+    (async () => {
+      const res = await listProducts(12);
+      setProducts(res.data);
+    })();
+  };
+
   return (
     <>
       <Navbar />
       <div className="shop">
         <div className="shop__left">
           <div className="shop__filterHead">Filter Products</div>
+          <button onClick={clearAll}>Clear All</button>
           <Accordion title="Price">
-            <Slider
-              min={10}
-              max={300}
-              value={price}
-              onChange={(_, newValue) => setPrice(newValue)}
-              onChangeCommitted={handlePrice}
-              valueLabelDisplay="auto"
-              valueLabelFormat={(x) => `$${x}`}
-            />
+            <div className="shop__filterSlider">
+              <Slider
+                min={0}
+                max={300}
+                value={price}
+                onChange={(_, newValue) => setPrice(newValue)}
+                onChangeCommitted={handlePrice}
+                valueLabelDisplay="on"
+                valueLabelFormat={(x) => `$${x}`}
+              />
+            </div>
           </Accordion>
           {categories && (
             <Accordion title="Category">
-              <div>
+              <div className="shop__filterCategory">
                 {categories.map((c, index) => {
                   return (
-                    <div key={index}>
-                      <Checkbox
-                        disableRipple={true}
-                        color="primary"
-                        value={c._id}
-                        onChange={handleCategory}
-                        checked={categoriesId.includes(c._id)}
-                      />
-                      <label htmlFor={"ch" + index}>{c.name}</label>
-                    </div>
+                    <FormControlLabel
+                      key={index}
+                      style={{
+                        paddingInline: ".75rem",
+                      }}
+                      control={
+                        <Checkbox
+                          disableRipple={true}
+                          color="primary"
+                          value={c._id}
+                          name={"ch" + index}
+                          onChange={handleCategory}
+                          size="small"
+                          checked={categoriesId.includes(c._id)}
+                        />
+                      }
+                      label={c.name}
+                    />
                   );
                 })}
               </div>
@@ -189,58 +210,66 @@ const Shop = () => {
                 value={value}
                 size="large"
                 onChange={handleStarValue}
-                onChangeActive={(_, newHover) => {
-                  setHover(newHover);
-                }}
               />
-              {value !== null && (
-                <Box
-                  margin="0.25rem 0"
-                  textAlign="center"
-                  fontSize="0.8rem"
-                  ml={2}
-                >
-                  {labels[hover !== -1 ? hover : value]}
-                </Box>
-              )}
             </div>
           </Accordion>
           <Accordion title="Shipping">
-            <div>
-              <Radio
-                color="primary"
-                checked={shipping === "Yes"}
-                value="Yes"
-                onChange={handleShipping}
-                name="shipping"
+            <div className="shop__filterShipping">
+              <FormControlLabel
+                style={{
+                  paddingInline: ".75rem",
+                }}
+                control={
+                  <Radio
+                    color="primary"
+                    checked={shipping === "Yes"}
+                    value="Yes"
+                    onChange={handleShipping}
+                    name="shipping"
+                    size="small"
+                  />
+                }
+                label="Yes"
               />
-              <label>Yes</label>
-            </div>
-            <div>
-              <Radio
-                color="primary"
-                checked={shipping === "No"}
-                value="No"
-                onChange={handleShipping}
-                name="shipping"
+              <FormControlLabel
+                style={{
+                  paddingInline: ".75rem",
+                }}
+                control={
+                  <Radio
+                    color="primary"
+                    checked={shipping === "No"}
+                    value="No"
+                    onChange={handleShipping}
+                    name="shipping"
+                    size="small"
+                  />
+                }
+                label={"No"}
               />
-              <label>No</label>
             </div>
           </Accordion>
           <Accordion title="Colors">
-            <div>
+            <div className="shop__filterColors">
               {colorsData.map((c, index) => {
                 return (
-                  <div key={index}>
-                    <Radio
-                      disableRipple={true}
-                      color="primary"
-                      value={c}
-                      onChange={handleColor}
-                      checked={color === c}
-                    />
-                    <label>{c}</label>
-                  </div>
+                  <FormControlLabel
+                    style={{
+                      paddingInline: ".75rem",
+                    }}
+                    key={index}
+                    control={
+                      <Radio
+                        disableRipple={true}
+                        color="primary"
+                        value={c}
+                        onChange={handleColor}
+                        checked={color === c}
+                        size="small"
+                      />
+                    }
+                    label={c}
+                  />
                 );
               })}
             </div>
@@ -255,7 +284,7 @@ const Shop = () => {
             <div className="shop__warning">
               <WarningDiv
                 message={
-                  displaySearch
+                  text !== ""
                     ? "Sorry, No Product Found Matching Your Search"
                     : "Sorry, No Products Found"
                 }
